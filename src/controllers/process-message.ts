aa1f04958df46873d551e394327532e15efcfdfb
Dmitriy Skrylnikov <skrylnikovd@ya.ru>
2020-08-02 12:19:43 +0300

    fix

--- src/controllers/process-message.ts
+++ src/controllers/process-message.ts
@@ -7,14 +7,19 @@ import { activateWordList, openWeatherToken } from '../config';
 import { processTextMessage } from './text-message';
 
 export const processMessageController = async (ctx: Context) => {
-  if (!ctx.from || !ctx.from.id || !ctx.chat || !ctx.chat.id || !ctx.message || ctx.message.forward_from) {
-    return;
-  }
-  const message = ctx.message;
+  try {
 
-  const { text } = message;
-
-  if (text) {
-    processTextMessage(ctx);
+    if (!ctx.from || !ctx.from.id || !ctx.chat || !ctx.chat.id || !ctx.message || ctx.message.forward_from) {
+      return;
+    }
+    const message = ctx.message;
+    
+    const { text } = message;
+    
+    if (text) {
+      processTextMessage(ctx);
+    }
+  } catch (e) {
+    console.error(e);
   }
 }
