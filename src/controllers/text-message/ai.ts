13d8e01e5e4cbfb6bfec137be64788a5e0bfe28f
Dmitriy Skrylnikov <dskrylnikov@samokat.ru>
2023-03-28 00:09:41 +0300

    fix

--- src/controllers/text-message/ai.ts
+++ src/controllers/text-message/ai.ts
@@ -1,27 +1,27 @@
-import { Configuration, OpenAIApi } from "openai";
+// import { Configuration, OpenAIApi } from "openai";
 
 import { openAIToken } from '../../config'
 
-const configuration = new Configuration({
-    // organization: "YOUR_ORG_ID",
-    apiKey: openAIToken,
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//     // organization: "YOUR_ORG_ID",
+//     apiKey: openAIToken,
+// });
+// const openai = new OpenAIApi(configuration);
 
 export const askAI = async (text: string) => {
-  const result = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content: 'Ты умный помоник. Ты назван в честь ИО - спутника питера',
-      },
-      {
-        role: 'user',
-        content: text,
-      }
-    ],
-  });
+  // const result = await openai.createChatCompletion({
+  //   model: 'gpt-3.5-turbo',
+  //   messages: [
+  //     {
+  //       role: 'system',
+  //       content: 'Ты умный помоник. Ты назван в честь ИО - спутника питера',
+  //     },
+  //     {
+  //       role: 'user',
+  //       content: text,
+  //     }
+  //   ],
+  // });
 
-  return result.data.choices[0].message?.content;
+  // return result.data.choices[0].message?.content;
 };
