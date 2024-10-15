38c634032ea73158162d302da671d26712bc0274
Dmitriy Skrylnikov <skrylnikovd@ya.ru>
2024-10-16 02:18:54 +0300

    fix

--- src/controllers/process-message.ts
+++ src/controllers/process-message.ts
@@ -8,37 +8,41 @@ import { aiController } from "../ai";
 export const processMessageController = new Composer<BotContext>();
 
 processMessageController.on(":text", async (ctx) => {
-  console.log("processMessageController");
-  await Promise.all([
-    saveChat(ctx.chat),
-    saveUser(ctx.from!),
-    saveUser(ctx.me),
-  ]);
+  try {
+    console.log("processMessageController");
+    await Promise.all([
+      saveChat(ctx.chat),
+      saveUser(ctx.from!),
+      saveUser(ctx.me),
+    ]);
 
-  const chat = await prisma.chat.findUnique({
-    where: {
-      id: ctx.chatId,
-    },
-  });
-
-  if (chat?.greeting) {
-    await prisma.message.create({
-      data: {
-        id: ctx.msg.message_id,
-        chatId: ctx.chatId,
-        senderId: ctx.from!.id,
-        replyToMessageId: ctx.msg?.reply_to_message?.message_id,
-        sentAt: new Date(ctx.msg.date * 1000),
-        text: ctx.msg.text,
-        messageType: "TEXT",
+    const chat = await prisma.chat.findUnique({
+      where: {
+        id: ctx.chatId,
       },
     });
-  }
 
-  if (
-    ctx.msg.text.toLowerCase().startsWith("ио") ||
-    ctx.msg.reply_to_message?.from?.id === ctx.me.id
-  ) {
-    await aiController(ctx);
+    if (chat?.greeting) {
+      await prisma.message.create({
+        data: {
+          id: ctx.msg.message_id,
+          chatId: ctx.chatId,
+          senderId: ctx.from!.id,
+          replyToMessageId: ctx.msg?.reply_to_message?.message_id,
+          sentAt: new Date(ctx.msg.date * 1000),
+          text: ctx.msg.text,
+          messageType: "TEXT",
+        },
+      });
+    }
+
+    if (
+      ctx.msg.text.toLowerCase().startsWith("ио") ||
+      ctx.msg.reply_to_message?.from?.id === ctx.me.id
+    ) {
+      await aiController(ctx);
+    }
+  } catch (error) {
+    console.error(error);
   }
 });
